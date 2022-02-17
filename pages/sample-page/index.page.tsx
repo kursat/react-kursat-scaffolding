3c89ef1fc2131f3b9b7b01103d45afd47961c3ea
Kürşat Yiğitoğlu <kursat.yigitoglu@gmail.com>
2022-02-17 17:42:58 +0300

    rollbar configuration

--- pages/sample-page/index.page.tsx
+++ pages/sample-page/index.page.tsx
@@ -1,12 +1,25 @@
 import Head from 'next/head'
-import React, { useCallback } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import Button from '../../components/core/Button/Button'
 import useAppDispatch from '../../hooks/useAppDispatch'
 import useAppSelector from '../../hooks/useAppSelector'
 import useMovies from '../../hooks/useMovies'
 import { decrement, increment, incrementByAmount } from '../../store/counter'
 
+import Rollbar from 'rollbar'
+
 const SamplePage = () => {
+  const [rollbar, setRollbar] = useState<Rollbar | null>(null)
+  useEffect(() => {
+    const instance = new Rollbar({
+      accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
+      captureUncaught: true,
+      captureUnhandledRejections: true,
+    })
+
+    setRollbar(instance)
+  }, [])
+
   const dispatch = useAppDispatch()
   const count = useAppSelector((state) => state.counter.value)
 
@@ -25,6 +38,10 @@ const SamplePage = () => {
     dispatch(incrementByAmount(-5))
   }, [dispatch])
 
+  const createError = useCallback(() => {
+    throw new Error('Kursat codebase is testing rollbar')
+  }, [])
+
   return (
     <>
       <Head>
@@ -41,6 +58,10 @@ const SamplePage = () => {
       </Button>
 
       <pre>{JSON.stringify(movies, null, 2)}</pre>
+
+      <h1>Rollbar</h1>
+
+      <Button onClick={createError}>Throw Error</Button>
     </>
   )
 }
